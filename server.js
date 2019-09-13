7072d2700f2ade5ce0dae66414aa1d2ba315ef94
Baron <36326959+Baron010@users.noreply.github.com>
2019-09-13 22:51:39 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html'); 
+});
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+// عشان تخلي البوت 24 ساعه
+// ولاتنسى تسوي خطوات ال uptimerobot
+const http = require('http');
+app.get("/", (request, response) => {
+  response.sendStatus(200); 
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://reapers-javax.glitch.me`); //replace jagroshmusicbot with your project name بدل jagroshmusicbot بي اسم البروجكت حقك
+}, 280000); 
+
+var shell = require('shelljs');
+  
+shell.exec('java -jar JMusicBot-0.2.5.jar')
